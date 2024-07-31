55b761b81e6dd8465ad5a6d4cf1bd07d058afe9f
Matt Miles <matt_miles2@live.co.uk>
2024-07-31 13:46:08 +0000

    Add arrow function challenge

--- arrow-functions.js
+++ arrow-functions.js
@@ -41,3 +41,43 @@ const returnMultipleLines = () => (
     </p>`
 )
 console.log(returnMultipleLines());
+
+// ARROW FUNCTIONS CHALLENGE:
+
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'maths','english', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', english: 'Joan', art: 'Simon'},
+        results: {science: 93, maths: 95, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 79, art: 95},
+    }
+];
+
+const averagePoints = (arr, subject) => {
+    let totalMarks = 0;
+    let studentCount = 0;
+    
+    arr.forEach(student => {
+        if (student.results.hasOwnProperty(subject)) {
+            totalMarks += student.results[subject];
+            studentCount++;
+        }
+    });
+    
+    return studentCount > 0 ? totalMarks / studentCount : 0;
+};
+
+let averageMarks = averagePoints(students, 'maths');
+console.log(averageMarks);
