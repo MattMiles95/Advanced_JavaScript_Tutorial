74042ff8015719b3f3258c70ff9a7c2462c8eedc
Matt Miles <matt_miles2@live.co.uk>
2024-07-31 18:28:08 +0000

    Add destructuring challenge

--- destructuring.js
+++ destructuring.js
@@ -58,3 +58,41 @@ let { brian, anna, ...rest } = favouriteFoods;
 console.log(brian);
 console.log(anna);
 console.log(rest);
+
+// DESTRUCTURING CHALLENGE
+
+let students = [
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'John',
+        subjects: ['art', 'cad', 'english', 'maths', 'science'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, maths: 77, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+const makeList = (arr, student) => {
+    const studentName = arr.find(studentName => studentName.name === student);
+    return studentName ? studentName.subjects : [];
+};
+
+let [first, second, ...other] = makeList(students, "John");
+
+console.log(first, second, other);
